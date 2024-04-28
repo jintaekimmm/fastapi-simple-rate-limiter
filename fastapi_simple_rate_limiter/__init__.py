07eedfb309d4c7921e2f643a819af6ed5725bed6
jintaekimmm <jintae.kimmm@gmail.com>
2024-04-28 22:51:12 +0900

    Update project urls

--- fastapi_simple_rate_limiter/__init__.py
+++ fastapi_simple_rate_limiter/__init__.py
@@ -8,4 +8,4 @@ __all__ = [
     "RateLimitException"
 ]
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
--- setup.py
+++ setup.py
@@ -3,14 +3,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="fastapi_simple_rate_limiter",
-    version="0.0.3",
+    version="0.0.4",
     description="Rate limiter to limit the number of API requests in FastAPI",
     author="jintaekimmm",
     author_email="jintae.kimmm@gmail.com",
     url="https://github.com/jintaekimmm/fastapi-simple-rate-limiter",
-    project_urls={
-        'Github': 'https://github.com/jintaekimmm/fastapi-simple-rate-limiter',
-    },
     install_requires=["redis"],
     packages=find_packages(exclude=[]),
     keywords=["fastapi-simple-rate-limiter", "fastapi_simple_rate_limiter", "rate limiter", "rate limit", "fastapi rate limit", "simple rate limit"],
